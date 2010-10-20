e745aa80bed986f7dc5c6acb398fafa2d8a35bcf
David Bushong <dbushong@mashlogic.com>
2010-10-20 14:37:14 -0700

    add -o flag, get of argument replacing

--- pcat.c
+++ pcat.c
@@ -3,11 +3,15 @@
 #include <stdio.h>
 #include <sys/select.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define DEF_PROCS 2
 #define MAX_PROCS 32
 
 #define MAX_LINE_BYTES 65535
+#define MAX_FILE_LEN   1024
 
 #define CHILD_READER  pipe_in_out[0]
 #define PARENT_WRITER pipe_in_out[1]
@@ -21,28 +25,32 @@ void check_fail(int st, char *err_msg) {
 
 void usage() {
   fprintf(stderr,
-"usage: pcat [-p num-procs] cmd [cmd-arguments]\n"
+"usage: pcat [-o output-template] [-p num-procs] cmd [cmd-arguments]\n"
 "       cmd: stdin line-level parallelism: cmd and any arguments given at the\n"
 "             end of the command line are invoked in parallel, passed some\n" 
-"             subset of standard input lines.  Any strings containing %%%%\n"
-"             will have the process number substituted.\n"
+"             subset of standard input lines.\n"
+"       -o: specify file to write output to; an instance of %%02d will be\n"
+"           replaced with the process number (01-<num-procs>)\n"
 "       -p: specify number of parallel processes (default: %d)\n", DEF_PROCS
   );
   exit(1);
 }
 
 int main(int argc, char *argv[]) {
-  int num_procs = 0;
-  char **cmd, line[MAX_LINE_BYTES], *start, num_buf[3];
-  int i, j, max_fd = 0, done, pipe_in_out[2], fd[MAX_PROCS], opt, cmdc;
+  int num_procs = 0, out_fd;
+  char **cmd, line[MAX_LINE_BYTES], *out_tmpl = NULL, out_file[MAX_FILE_LEN];
+  int i, max_fd = 0, done, pipe_in_out[2], fd[MAX_PROCS], opt, cmdc;
   pid_t child_pid;
   size_t len;
   fd_set wfds;
 
-  while ((opt = getopt(argc, argv, "hp:")) != -1) {
+  while ((opt = getopt(argc, argv, "hp:o:")) != -1) {
     switch (opt) {
       case 'p':
         num_procs = atoi(optarg);
+        break;
+      case 'o':
+        out_tmpl = optarg;
         break;
       default:
         usage();
@@ -72,11 +80,15 @@ int main(int argc, char *argv[]) {
       check_fail(close(PARENT_WRITER), "Couldn't close parent writer in child");
       check_fail(dup2(CHILD_READER, STDIN_FILENO), "Couldn't set file desc");
 
-      // replace %% with the 01-<num-procs>
-      check_fail(sprintf(num_buf, "%02d", i+1), "formatting num failed");
-      for (j = 0; j < cmdc; j++)
-        while ((start = strstr(cmd[j], "%%")) != NULL) 
-          memcpy(start, num_buf, 2);
+      // output file handling
+      if (out_tmpl != NULL) {
+        check_fail(sprintf(out_file, out_tmpl, i+1), 
+            "formatting outfile failed");
+        check_fail(out_fd = open(out_file, O_CREAT | O_WRONLY, 0666),
+            "failed to open output file for writing");
+        check_fail(dup2(out_fd, STDOUT_FILENO), 
+            "couldn't redirect child output");
+      }
 
       if (execvp(cmd[0], cmd) < 0) {
         perror("Couldn't exec");
